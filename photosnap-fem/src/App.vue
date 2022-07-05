70a8997739d4cb8ab5ab82da660dda66cb3ecb46
Darryl <darryl.a.nicholson@gmail.com>
2022-07-05 06:22:20 -0700

    Added Vue 3 Yay

---
+++ photosnap-fem/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>{{title}}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        title: 'my first post'
+      }
+    },
+    methods: {
+      handleClick() {
+        console.log(this.$refs.name)
+        this.$refs.name.classList.add('foo');
+        this.$refs.name.focus()
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ photosnap-fem/src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>COntent here</p>
+        </div>
+    </div>
+</template>
+
+<style> 
+    .modal {
+        widht: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+
+    .backdrop {
+        top: 0;
+        position: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+</style>
