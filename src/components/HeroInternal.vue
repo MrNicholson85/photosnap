211e683df4f6ff49580bacaba03b7bd6db01cc92
Darryl <darryl.a.nicholson@gmail.com>
2022-07-30 16:53:00 -0700

    stories page update

---
+++ src/components/HeroInternal.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="hero-internal">
+        <div class="hero-internal__Img" :style="{ backgroundImage: `url(${internalHeroImage})` }">
+            <div class="hero-internal__content">
+                <slot class="hero-internal__sub-title" name="subTitle">Module Sub Title</slot>
+                <slot class="hero-internal__title" name="title">Module Title</slot>
+                <slot class="hero-internal__author" name="author">Module Author</slot>
+                <slot class="hero-internal__content" name="content">Module Content</slot>
+                <slot class="hero-internal__link" name="link">Module Link</slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import internalHeroImage from '../assets/images/moon-of-appalacia.jpg';
+
+    export default {
+        data () {
+            return {
+                internalHeroImage,
+            }
+        },
+        methods: {
+            isMobile() {
+            return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+                navigator.userAgent
+            );
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    @import '../assets/styles/variable.scss';
+    
+    .hero-internal {
+        &__Img {
+            height: 650px;
+            width: $desktop;
+            background-repeat: no-repeat;
+            background-size: cover;
+            margin: 0 auto;
+            padding: 122px 112px;
+        }
+
+        &__content {
+            width: 406px;
+            color: $pureWhite;
+
+            p{
+                display: block;
+                font-size: 13px;
+            }
+        }
+    }
+</style>
--- src/views/Stories.vue
+++ src/views/Stories.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="stories">
-    <h1>This is an Stories page</h1>
+  <div class="stories-page">
+      <heroInternal>
+        <template #subTitle>
+          <h4>LAST MONTH’S FEATURED STORY</h4>
+        </template>
+        <template #title>
+          <h1>
+            HAZY FULL MOON OF APPALACHIA
+          </h1>
+        </template>
+        <template #author>
+          <p>
+            March 2nd 2020 <span>by John Appleseed</span>
+          </p>
+        </template>
+        <template #content>
+          <p>The dissected plateau area, while not actually made up of geological mountains, is popularly called "mountains," especially in eastern Kentucky and West Virginia, and while the ridges are not high, the terrain is extremely rugged.</p>
+        </template>
+        <template #link>
+          <div>
+            READ THE STORY
+          </div>
+        </template>
+      </heroInternal>
   </div>
 </template>
+
+<script>
+import heroInternal from '@/components/HeroInternal';
+
+export default {
+  components: { heroInternal }
+}
+</script>
+<style lang="scss">
+  
+</style>
