5e9c50c33f63c964e3bdf8c909ccef2bc15a2e76
Darryl <darryl.a.nicholson@gmail.com>
2022-08-10 11:55:13 -0700

    compare component wip

---
+++ src/components/compareServices.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="compareServices">
+        <div class="compareServices__titles">
+            <span>The Features</span>
+            <span>Basic</span>
+            <span>Pro</span>
+            <span>Business</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+@import '../assets/styles/variable.scss';
+
+.compareServices {
+    width: $mobile;
+    margin: 0 auto;
+    padding-top: 80px;
+
+    @media(min-width: $tablet) {
+        width: 731px;
+    }
+
+    &__titles {
+        display: grid;
+        grid-template-columns: auto 140px 140px 140px;
+        border-bottom: $pureBlack 1px solid;
+        padding-bottom: 23px;
+    }
+}
+</style>
--- src/views/Pricing.vue
+++ src/views/Pricing.vue
@@ -15,16 +15,22 @@
       </template>
     </internalHero>
     <pricingTable/>
+    <CompareServices/>
+    <ctaBanner/>
   </div>
 </template>
 
 <script>
 import internalHero from '@/components/Hero.vue';
 import pricingTable from '@/components/PricingTier.vue';
+import compareServices from '@/components/compareServices.vue';
+import ctaBanner from '@/components/ctaBanner.vue';
+
 import heroImage from '../assets/images/pricing-hero.jpg';
+import CompareServices from '../components/compareServices.vue';
 
   export default {
-    components: {internalHero, pricingTable},
+    components: { internalHero, pricingTable, ctaBanner, compareServices, CompareServices },
     data() {
       return {
         heroImage        
